029bc3ebc0723b668d366ebfae88f8cf6b299f52
Chris Ravosa <christopher.ravosa99@gmail.com>
2026-07-17 00:27:35 +0100

    Add drag icon

--- Assets/Scripts/Gameplay/Draggable.cs
+++ Assets/Scripts/Gameplay/Draggable.cs
@@ -1,22 +1,40 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [SerializeField] private Canvas _dragCanvas;
+    [SerializeField] private Image _dragIcon;
+
+    private RectTransform _draggingPlane;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        _dragIcon.enabled = true;
+        SetDraggedPosition(eventData);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        SetDraggedPosition(eventData);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        _dragIcon.enabled = false;
+    }
+
+    private void SetDraggedPosition(PointerEventData data)
+    {
+        if (data.pointerEnter != null && data.pointerEnter.transform as RectTransform != null)
+            _draggingPlane = data.pointerEnter.transform as RectTransform;
+
+        var dragIconRt = _dragIcon.rectTransform;
+
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(_draggingPlane, data.position, data.pressEventCamera, out Vector3 globalMousePos))
+        {
+            dragIconRt.position = globalMousePos;
+            dragIconRt.rotation = _draggingPlane.rotation;
+        }
     }
 }
